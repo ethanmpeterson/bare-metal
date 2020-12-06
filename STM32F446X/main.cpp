67d4a931e70a86105cd687ea6ab78ece5f44e752
Ethan Peterson <17emp5@queensu.ca>
2020-12-05 22:44:34 -0500

    add FPU test code

--- STM32F446X/main.cpp
+++ STM32F446X/main.cpp
@@ -2,10 +2,12 @@
 
 std::unique_ptr<int> a = std::make_unique<int>();
 volatile int *b;
+volatile float fTestVal = 0;
 
 int main(void) {
   b = a.get();
 	for(;;) {
 		(*a)++;
+    fTestVal += 0.01;
 	}
 }
