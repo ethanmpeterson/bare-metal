4e3f4a5177187632c0149cecadca8fbe395f504e
ethan <17emp5@queensu.ca>
2020-12-27 17:01:51 -0500

    add clock init code selected max speed of 180Mhz

--- STM32F446X/clock.c
+++
@@ -1,5 +0,0 @@
-// TODO: Implement Clock Initialization
-// NOTE: This will be different for each PCB / Clock Source
-void SystemInit(void) {
-
-}
--- STM32F446X/main.cpp
+++ STM32F446X/main.cpp
@@ -1,14 +1,33 @@
 #include "stm32f4xx.h"
 #include <memory>
 
+#define LED_PORT GPIOB
+#define LED_DDR GPIOB
+#define LED 1
+
+void delay() {
+    for (volatile long long i = 0; i < 500000; i++);
+}
+
 std::unique_ptr<int> a = std::make_unique<int>();
 volatile int *b;
 volatile float fTestVal = 0;
 
+volatile int clockSpeed = SystemCoreClock;
+
 int main(void) {
-  b = a.get();
+	SystemCoreClockUpdate();
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN; // enable GPIO port B  clock
+    LED_PORT->MODER |= LED; // set bit 0 to output
+    LED_PORT->ODR = 0;
+
+    b = a.get();
+
 	for(;;) {
+		LED_PORT->ODR ^= LED;
+		delay();
+
 		(*a)++;
-    fTestVal += 0.01;
+    	fTestVal += 0.01;
 	}
 }
