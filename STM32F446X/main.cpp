41021b7ec02adbdcf2ca6104daf8c877f87a2a2b
ethan <17emp5@queensu.ca>
2020-12-27 20:17:50 -0500

    cleanup libraries folder

--- STM32F446X/main.cpp
+++ STM32F446X/main.cpp
@@ -15,6 +15,23 @@ volatile float fTestVal = 0;
 
 volatile int clockSpeed = SystemCoreClock;
 
+void UART_INIT() {
+	// fck is the peripheral clock
+	// pg 808 in the reference manual has baud rate selection info
+	
+	// General code outline
+	// set baud rate to 115200 baud
+	//	word length to 8 bits
+	// 1 stop bits
+	// no parity
+	// full duplex mode
+	// no hardware flow control
+	// 16 oversampling
+	// one bit sample disable
+	// no init advfeature
+	// SEE HAL INIT EXAMPLES
+}
+
 int main(void) {
 	SystemCoreClockUpdate();
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN; // enable GPIO port B  clock
