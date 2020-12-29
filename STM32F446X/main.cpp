282c066e9a300c40484e03fba9df425e390318f5
ethan <17emp5@queensu.ca>
2020-12-28 22:33:23 -0500

    working UART config for 115200 kb

--- STM32F446X/main.cpp
+++ STM32F446X/main.cpp
@@ -13,10 +13,8 @@ std::unique_ptr<int> a = std::make_unique<int>();
 volatile int *b;
 volatile float fTestVal = 0;
 
-volatile int clockSpeed = SystemCoreClock;
-
 // Configure USART2 which is connected to the blackmagic probe on the PMBoard
-void UART_INIT() {
+void USART2_INIT() {
 	// fck is the peripheral clock
 	// pg 808 in the reference manual has baud rate selection info
 
@@ -25,20 +23,50 @@ void UART_INIT() {
 	// in APB1 clock enable register, USART 2 enable is bit 17
 	RCC->APB1ENR |= RCC_APB1ENR_USART2EN; // This def is same as 1 << 17
 
+	// Configure PA2 and PA3 and as Tx and Rx for the UART
+	// first enable clock for the GPIO pins 
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN; // same as 1 << 0 (enable bit for GPIOA is in bit 0)
+
+	// set the alternate function reister accordingly to get UART setup on PA2 and PA3
+	GPIOA->AFR[0] = 0x0700; // index 0 for lower 32 bits of AFR register and UART is in bit 7
+	// Mode setting for PA2 in bits 4 and 5. bit 4 should be 0 and bit 5 should be 1
+	GPIOA->MODER |= GPIO_MODER_MODER2_1; // same as 1 << 5
+	// PA3 also needs to be in alternate function mode. set bit 7
+	
+	//GPIOA->MODER |= GPIO_MODER_MODER3_1; // same as 1 << 7
+
+	// Now set the baud rate
+	// desired rate is 115200 baud
+	// USART DIV = fck/(8 * baud) = 48.828125
+	// DIV_FRACTION = 8 * 0.828125 = 6.625 -> round it = 7 = 0x7
+	// DIV_MANTISSA = 48 = 0x30
+	// BRR value = 0x307
+	// USART2->BRR = 0x0307; // 115200
+	USART2->BRR = 0x019A;
+
+	// USART2->BRR = (((uartdiv / 16 ) << 4 ) | ((uartdiv % 16 )));
+	// SET USART enable, transmit enable, receive enable in control register 1
+	USART2->CR1 |= USART_CR1_TE; // only enable transmit for now
+	// USART2->CR1 |= USART_CR1_OVER8;
+	USART2->CR1 |= USART_CR1_UE;
+}
+
+void USART_WRITE(int c) {
+	while (!(USART2->SR & USART_SR_TXE)); // check that there is buffer space first
+	USART2->DR = (c & 0xFF);
 }
 
 int main(void) {
 	SystemCoreClockUpdate();
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN; // enable GPIO port B  clock
     LED_PORT->MODER |= LED; // set bit 0 to output
-    LED_PORT->ODR = 0;
-
+    LED_PORT->ODR = 1;
     b = a.get();
+	USART2_INIT();
 
 	for(;;) {
-		LED_PORT->ODR ^= LED;
+		USART_WRITE(65);
 		delay();
-
 		(*a)++;
     	fTestVal += 0.01;
 	}
